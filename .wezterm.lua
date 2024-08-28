06b6157a756ec42ac2f19ec26b08652c356218d9
nixmage <puneetpoojary@gmail.com>
2024-08-28 20:25:07 +0530

    update wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,8 +7,9 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-local canonical_solarized = require "./.config/wezterm/canonical_solarized"
-canonical_solarized.apply_to_config(config)
-
+--local canonical_solarized = require "canonical_solarized"
+--canonical_solarized.apply_to_config(config)
+config.color_scheme = 'Solarized (light) (terminal.sexy)'
+config.bold_brightens_ansi_colors = "No"
 -- and finally, return the configuration to wezterm
 return config
